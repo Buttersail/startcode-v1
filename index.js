c2b422da43e5a714af1c495774c9440e58b86035
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2022-03-09 12:14:01 +0100

    add setup for thursday/friday lecutre

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-import { renderTemplate, setActive, showPage } from "./utils.js"
-import { setupP2Handlers } from "./js-for-pages/page2.js"
-import { setUpPage3Handlers, getAllUsers } from "./js-for-pages/page3.js"
-
-
-
+import { renderTemplate, setActive, showPage } from './utils.js'
+import { setupP2Handlers } from './js-for-pages/page-see-cars.js'
+import { setUpPage3Handlers, getAllUsers } from './js-for-pages/page-add-cars.js'
 
 function renderMenuItems(evt) {
   const element = evt.target
   setActive(element)
-  const id = element.id;
-  renderTemplate(id)  //This setups the HTML for the page
+  const id = element.id
+  renderTemplate(id) //This setups the HTML for the page
   switch (id) {
     //Here you can execute JavaScript for the selected page
-    case "page-1": {
+    case 'page-about': {
       break
     }
-    case "page-2": {
+    case 'page-see-cars': {
       setupP2Handlers()
       break
     }
-    case "page-3": {
+    case 'page-add-cars': {
       setUpPage3Handlers()
       getAllUsers()
       break
@@ -27,9 +24,5 @@ function renderMenuItems(evt) {
   }
 }
 
-document.getElementById("menu").onclick = renderMenuItems;
-showPage("page-1") //Set the default page to render
-
-
-
-
+document.getElementById('menu').onclick = renderMenuItems
+showPage('page-1') //Set the default page to render
